d21c363e0b6d802bd446d2a3073ce5cdc6854a9f
pankace <44595774+pankace@users.noreply.github.com>
2025-04-16 17:24:15 +0700

    init -

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,3 @@
-
-
 output "bigquery_dataset" {
   description = "BigQuery dataset for MT5 data"
   value       = data.google_bigquery_dataset.mt5_trading.dataset_id
@@ -11,17 +9,16 @@ output "mt5_function_url" {
 }
 
 output "positions_table" {
-  description = "Existing BigQuery table for positions data"
-  value       = "${data.google_bigquery_dataset.mt5_trading.dataset_id}.${data.google_bigquery_table.positions.table_id}"
+  description = "BigQuery table for positions data"
+  value       = "${data.google_bigquery_dataset.mt5_trading.dataset_id}.${google_bigquery_table.positions.table_id}"
 }
 
 output "transactions_table" {
-  description = "Existing BigQuery table for transactions data"
-  value       = "${data.google_bigquery_dataset.mt5_trading.dataset_id}.${data.google_bigquery_table.transactions.table_id}"
+  description = "BigQuery table for transactions data"
+  value       = "${data.google_bigquery_dataset.mt5_trading.dataset_id}.${google_bigquery_table.transactions.table_id}"
 }
 
 output "prices_table" {
-  description = "Existing BigQuery table for price updates data"
-  value       = "${data.google_bigquery_dataset.mt5_trading.dataset_id}.${data.google_bigquery_table.price_updates.table_id}"
+  description = "BigQuery table for price updates data"
+  value       = "${data.google_bigquery_dataset.mt5_trading.dataset_id}.${google_bigquery_table.price_updates.table_id}"
 }
-
